6423fb48e86678bcfe22ea27a3a9c727a54722c3
Artur D. Nasyrov <nasyrovartur151998@gmail.com>
2024-12-15 12:11:00 +0300

    HttplibServer Hello world

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include "httplib.h"
 #include <nlohmann/json.hpp>
 int main(){
-    std::cout<<"main.cpp";
+    httplib:: Server server;
+
+    server.Get("/hello", [](const httplib::Request&, httplib::Response& res)
+                                {
+                                res.set_content("Hello world! \n", "text/plain");
+                                } );
+    server.listen("0.0.0.0", 8080);
+
+
     return 0;
 }

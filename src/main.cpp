0a30da1468ace624c8f01ec0e153dbd3ae557c2b
Artur D. Nasyrov <nasyrovartur151998@gmail.com>
2024-12-16 06:39:43 +0300

    Hello world add the comments in the code

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include "httplib.h"
 #include <nlohmann/json.hpp>
 int main(){
+    //object of server
     httplib:: Server server;
 
+    // path registration for get-request
+    // there is the lambda function for request processing
     server.Get("/hello", [](const httplib::Request&, httplib::Response& res)
                                 {
-                                res.set_content("Hello world! \n", "text/plain");
+                                res.set_content("Hello world! \n", "text/plain"); //set the answer for get request
                                 } );
+
     server.listen("0.0.0.0", 8080);
 
 

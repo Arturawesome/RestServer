b904870a657f36274f83b88863be9c54490f90db
ArturAwesome <nasyrovartur151998@gmail.com>
2024-12-17 13:16:52 +0300

    Finish work with main4.cpp

--- src/main4.cpp
+++ src/main4.cpp
@@ -8,6 +8,7 @@ const std::string ROOT_DIR = "../data";
 
 void get_list_function(const httplib::Request& req, httplib::Response& res);
 void get_file_info(const httplib::Request& req, httplib::Response& res);
+void put_file_func(const httplib::Request& req, httplib::Response& res);
 
 int main(){
     httplib::Server server;
@@ -17,7 +18,11 @@ int main(){
 
     server.Get("/list", get_list_function);
     server.Get(R"(/file/(.+))", get_file_info);
+    server.Put(R"(/file/(.+))", put_file_func);
 
+    if (!std::filesystem::exists(ROOT_DIR)) {
+        std::filesystem::create_directory(ROOT_DIR);
+    }
 
     server.listen("0.0.0.0", 8080);
     return 0;
@@ -26,10 +31,25 @@ int main(){
 
 
 
+void put_file_func(const httplib::Request& req, httplib::Response& res){
+    std::string file_name = req.matches[1].str();
+    std::string file_path = std::filesystem::path(ROOT_DIR)/file_name;
+
+    std::ofstream f(file_path, std::ios::binary);
+    if (f){
+        f<<req.body;
+        res.set_content("File is created", "text/plain");
+        return;
+    }
+    res.status = 500;
+    res.set_content("Error in file creating", "text/plain");
+
+}
 
 void get_file_info(const httplib::Request& req, httplib::Response& res){
     std::string file_name = req.matches[1].str();
     auto file_path = std::filesystem::path(ROOT_DIR)/file_name;
+    std::cout << "Requested file: " << file_path << std::endl;
 
     if(std::filesystem::exists(file_path) && std::filesystem::is_regular_file(file_path)){
         std::ifstream f(file_path, std::ios::binary);
@@ -46,7 +66,6 @@ void get_file_info(const httplib::Request& req, httplib::Response& res){
         res.status = 404;
         res.set_content("File does not exist", "text/plain");
     }
-
 }
 
 void get_list_function(const httplib::Request& req, httplib::Response& res){
